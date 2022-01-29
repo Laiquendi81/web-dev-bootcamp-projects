77a426921a7c8ae2c9007d75e9fd188033a2b2a0
Laiquendi81 <new_imladris@hotmail.com>
2022-01-29 11:14:24 +0000

    update

--- week_three/digital_clock_challenge/script.js
+++ week_three/digital_clock_challenge/script.js
@@ -1,8 +1,19 @@
 //Hints
 //function setTime() {
+
+let time = document.getElementById('time');
    
-    
+function setTime() {
+    const clock = new Date();
+
+    let hours = clock.getHours();
+    let minutes = clock.getMinutes();
+    let seconds = clock.getSeconds();
+
+    time.innerHTML = hours + "." + minutes + "." + seconds;
+}
     
    
 //}
 //setTime();
+setInterval(setTime, 1000);
