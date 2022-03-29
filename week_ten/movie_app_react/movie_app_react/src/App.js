cdf2537a5a10cc6b00f9bc646c644564ac09b6a4
Laiquendi81 <new_imladris@hotmail.com>
2022-03-29 20:44:26 +0100

    update

--- week_ten/movie_app_react/movie_app_react/src/App.js
+++ week_ten/movie_app_react/movie_app_react/src/App.js
@@ -7,17 +7,44 @@ const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a
 function App() {
 
   const [ movies, setMovies] = useState([]);
+  const [ searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     fetch(FEATURED_API).then(res => res.json())
     .then(data => {
-      console.log(data);
       setMovies(data.results);
     });
   
   }, []);
 
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    if(searchTerm) {
+      fetch(SEARCH_API + searchTerm).then(res => res.json())
+      .then((data) => {
+      setMovies(data.results);
+      });
+
+    setSearchTerm('');
+    }
+
+    
+  }
+    
+
+
+  const handleOnChange = (e) => {
+     setSearchTerm(e.target.value);
+  }
+
   return(
+    <>
+      <header>
+        <form onSubmit={handleOnSubmit}>
+          <input className='search' type='text' placeholder='Search...' value={searchTerm} onChange={handleOnChange}/>
+        </form>
+      </header>
     <div className="movie-container">
       {
         movies.length > 0 && movies.map(movie => (
@@ -25,6 +52,8 @@ function App() {
         ))
       }
     </div>
+    </>
+    
   )
 }
 
